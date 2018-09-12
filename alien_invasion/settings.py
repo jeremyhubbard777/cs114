82ac06fb5f96026c5ebad9bce502b4e48fd14947
Student <student@2024-01-203-12.aii.stu>
2018-09-12 16:31:11 -0700

    updates

---
+++ alien_invasion/settings.py
@@ -0,0 +1,17 @@
+class Settings():
+    """A class to store all settings for Alien Invasion."""
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 0)
+        
+        # Ship settings
+        self.ship_speed_factor = 4
+        
+        #bullet settings
+        self.bullet_speed_factor = 20
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 250, 250, 250
